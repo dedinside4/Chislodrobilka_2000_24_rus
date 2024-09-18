20fb9fdf759299005bd2f9c3c08bead95c251d0c
dedinside4 <svistov.mm@phystech.edu>
2024-09-18 17:07:42 +0300

    Начало...

---
+++ 12.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+
+
+
+fig, ax = plt.subplots()
+
+t = np.linspace(0, 200, 40000)
+a = 3
+b = 2
+d = np.linspace(0, 2 * np.pi, 1000)
+
+
+
+x = np.sin(a*t + d[0])
+y = np.sin(b*t)
+
+#print(x)
+
+
+line2 = ax.plot(x, y)[0]
+ax.set(xlim=[-1.1, 1.1], ylim=[-1.1, 1.1], xlabel='x', ylabel='y')
+ax.legend()
+
+
+def update(frame):
+    # for each frame, update the data stored on each artist.
+    
+    x = np.sin(a*t + d[frame])      
+
+    # update the line plot:
+    line2.set_xdata(x)
+    line2.set_ydata(y)
+    return (line2)
+
+
+ani = animation.FuncAnimation(fig=fig, func=update, frames=1000, interval=30)
+plt.show()
---
+++ 13.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+n = 4
+
+
+
+A = np.eye(n)*2
+
+it = np.nditer(a, flags=['multi_index'])
+
+print(A)
