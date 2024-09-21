5965028fabf16eab072b8982e31ad2b9e9a53151
dedinside4 <svistov.mm@phystech.edu>
2024-09-21 20:03:38 +0300

    Не гистограмма конечно, но точки самопересечения вроде кое-как ищет

--- 12.py
+++ 12.py
@@ -6,17 +6,15 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
-a = 3
-b = 2
-t = np.linspace(0, 2 * np.pi * (a - b), 40000)
-d = np.linspace(0, 0 * np.pi, 1000)
+a = 5
+b = 3
+t = np.linspace(0, 2 * np.pi, 1000)
+d = np.linspace(0, 2 * np.pi, 1000)
 
 
 
 x = np.sin(a*t + d[0])
 y = np.sin(b*t)
-
-#print(x)
 
 
 line2 = ax.plot(x, y)[0]
@@ -27,13 +25,23 @@ ax.legend()
 def update(frame):
     # for each frame, update the data stored on each artist.
     
-    x = np.sin(a*t + d[frame])      
-
+    x = np.sin(a*t[:frame + 1] + np.pi / 4)      
+    y = np.sin(b*t[:frame + 1])
     # update the line plot:
     line2.set_xdata(x)
     line2.set_ydata(y)
+    ax.set(xlim=[-1.1, 1.1], ylim=[-1.1, 1.1], xlabel= 'x', ylabel='y')
     return (line2)
 
+def lis_hist(a, b, d):
+    t = np.linspace(0, 2 * np.pi * (a - b), 1000)
+    x = np.sin(a*t + d)
+    y = np.sin(b*t)
+    np.stack(x, y)   
 
-ani = animation.FuncAnimation(fig=fig, func=update, frames=1000, interval=30)
+    xs = np.linspace(-1, 1, 1000)
+    
+    
+
+ani = animation.FuncAnimation(fig=fig, func=update, frames=1000, interval=1, repeat_delay=10000)
 plt.show()
--- 13.py
+++ 13.py
@@ -1,12 +1,38 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.animation as animation
 
 
-n = 4
+def lis_hist(a, b, d):
+
+    bitterness = 20000
+
+    t = np.linspace(0, 2 * np.pi, bitterness)
+    x = np.sin(a*t + d)
+    y = np.sin(b*t)
+
+    dots = np.stack((x, y), axis=1)
+    print(dots[0])    
+    
+    self_crossed_x = []
+    self_crossed_y = []
+    
+    i = 1
+    while i < bitterness - 1:
+        l = np.linalg.norm((dots[:i] - dots[i]), axis=1)
+        
+        l_min = l[-1] / 1.5
+        l = l[:-1]
+        
+        if np.any(l - l_min < 0):
+            self_crossed_x.append(dots[i][0])
+            self_crossed_y.append(dots[i][1])
+            i += 10
+        i += 1
+            
+        
+    plt.scatter(self_crossed_x, self_crossed_y)
+    plt.show()
 
 
-
-A = np.eye(n)*2
-
-it = np.nditer(a, flags=['multi_index'])
-
-print(A)
+lis_hist(5, 3, np.pi / 4)
