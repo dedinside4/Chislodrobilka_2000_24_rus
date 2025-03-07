1a451233841e563e7485380879b0bb22f52e5959
dedinside4 <svistov.mm@phystech.edu>
2025-03-08 01:37:24 +0300

    wk_03

--- wk_02_anal.py
+++ wk_02_anal.py
@@ -1,6 +1,7 @@
 from math import factorial
 from matplotlib import pyplot as plt
 import numpy as np
+import scipy.optimize as opt
 
 def C(N, k):
     return factorial(N) / factorial(k) / factorial(N - k)
@@ -10,20 +11,72 @@ def anal(K, N):
     q = 0
 
     for i in range(1, K):
-        q += (-1)**(i + 1) * C(K, i) * ((K - i) / K)**N
+        q += (-1)**(i + 1) * C(K, i) * ((K - i))**N
 
-    p = 1 - q
+    p = K**N - q
 
-    return p
+    return q
 
-K = 37
+def true_anal(N, *k):
+    K = 3
+    f = 1
+    for i in range(0, K):
+        f *= N - i
+    f *= 1
+    return f 
 
-Ns = np.array([i for i in range(1, 126)])
+K = 3
 
-plt.plot(Ns, anal(K, Ns))
+N = 24
 
+Ks = np.array([i for i in range(3, 17)])
+Ns = np.array(np.linspace(3, 27, 1000))
+
+#ks = opt.curve_fit(true_anal, Ns, anal(K, Ns), [1, 1, 1, 1, 1])[0]
+
+#print(ks)
+
+'''
+N!
+ ________
+
+K!(N-K)!
+'''
+
+plt.scatter(Ns, [true_anal(N) for N in Ns], color="red")
+
+plt.scatter(Ns, anal(K, Ns), color="green")
+'''
 with open("data.txt") as data_file:
-    data = [float(i) for i in data_file.read().split()]
-    plt.plot(Ns, data)    
+    data_preord = [float(i) for i in data_file.read().split()]
+    dots_mean = []
+    dots_std = []
+    for i in range(37):
+        dot = []   
+        for j in range(100):
+            dot.append(data_preord[37 * j + i])
+        dot = np.array(dot)
+        dots_mean.append(dot.mean())  
+        dots_std.append(dot.std()) 
 
+dots_mean = np.array(dots_mean)
+dots_std = np.array(dots_std)
+'''
+
+'''
+with open("data.txt") as data_file:
+    data = np.array([float(i) for i in data_file.read().split()])
+    
+    
+dots = np.array([i * 1000 for i in range(len(data))])
+plt.scatter(dots, abs(anal(K, 13) - data), color="green") 
+
+with open("data1.txt") as data_file:
+    data = np.array([float(i) for i in data_file.read().split()])
+    
+    
+dots = np.array([i * 1000 for i in range(len(data))])
+plt.scatter(dots, abs(anal(K, 13) - data), color="red")
+'''
+ 
 plt.show()
--- wk_02_prac.cpp
+++ wk_02_prac.cpp
@@ -13,40 +13,42 @@ int check(bool* sample, int K){
 int main(){
 
     std::ofstream out_file;
-    out_file.open("data.txt", std::ios::out | std::ios_base::trunc);
+    out_file.open("data1.txt", std::ios::out | std::ios_base::trunc);
+    //std::ofstream out_file_acc;
+    //out_file_acc.open("data_acc.txt", std::ios::out | std::ios_base::trunc);
 
     std::random_device r;
     std::default_random_engine e1(r());
 
-    int K = 37;
+    int K = 7;
     
     std::uniform_int_distribution<int> uniform_dist(0, K - 1);
     
-    int N_MAX = 125;    
+    int N_MAX = 37;    
     
     bool sample[K] = {true};
 
-    int total_iters = 100000;
+    int total_iters = 100000000;
 
     //float p[N_MAX];
-
-    for(int N = 0; N < N_MAX; N++){
-        int succeeded = 0; 
-        for(int iter = 0; iter < total_iters; iter++){          
-            for(int i = 0; i <= N; i++){
-                sample[uniform_dist(e1)] = true;
-            }
-            succeeded += check(sample, K);
-            for(int i = 0; i < K; i++){
-                sample[i] = false;
-            }     
+    //for(int N = 0; N < N_MAX; N++){
+    int N = 13;
+    int succeeded = 0; 
+    for(int iter = 0; iter < total_iters; iter++){          
+        for(int i = 0; i <= N; i++){
+            sample[uniform_dist(e1)] = true;
         }
+        succeeded += check(sample, K);
+        for(int i = 0; i < K; i++){
+            sample[i] = false;
+        }   
+        if(iter % 10000 == 0){
+            float p = (float)succeeded / (float)iter;
+            out_file<<p<<' '; 
+        }
+    }      
+    //}
 
-        float p = (float)succeeded / (float)total_iters;
-        out_file<<p<<' ';        
-    }
-    
-    
     
     return 0;
 }
